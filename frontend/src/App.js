a964a21ab28f4ec816e6939f4db1cbbfd501b2f8
BunnySunny24 <75733731+bunnysunny24@users.noreply.github.com>
2025-02-20 17:52:03 +0530

    src update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import VehicleDashboard from "./components/VehicleDashboard";
 import "./index.css"; // Ensure Tailwind CSS is loaded
@@ -11,3 +12,18 @@ function App() {
 }
 
 export default App;
+=======
+import React from "react";
+import VehicleDashboard from "./components/VehicleDashboard";
+import "./index.css"; // Ensure Tailwind CSS is loaded
+
+function App() {
+  return (
+    <div className="App">
+      <VehicleDashboard />
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> d445825 (Esp32 and graph)
